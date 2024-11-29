825aec7c9cc26349bad5dba80a332fd99e607eb8
Mayssabenchikh <abderrahmenmayssa@gmail.com>
2024-11-29 21:27:41 +0100

    first commit

--- navbar.php
+++ navbar.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Navbar Transparente</title>
     <link rel="stylesheet" href="navbar.css">
-</head>
+    <link rel="stylesheet"href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"/>
+</head >
 <body>
     <header>
         <nav class="navbar">
@@ -23,6 +24,30 @@
                 <li><a href="#">Logout</a></li>
             </ul>
         </nav>
+      <form action="search.php" method="get">   
+    <div class="search-bar">
+    <div class="dropdown">
+        <div id="drop-text" class="dropdown-text">
+          <span id="span">Everything</span>
+          <i id="icon" class="fa-solid fa-chevron-down"></i>
+        </div>
+        <ul id="list" class="dropdown-list">
+          <li class="dropdown-list-item" name="category">Catégorie</li>
+          <li class="dropdown-list-item" name="location">Lieu</li>
+        </ul>
+        
+      </div>
+     
+      <div class="search-box">
+        <input type="text" id="search-input" placeholder="Search anything..." />
+        <input type="datetime-local" name="event_date">
+        <button type="submit" class="fa-solid fa-magnifying-glass"></button>
+        
+      </div>
+      
+    </div></form>  
     </header>
+    <script src="script.js"></script>
+
 </body>
 </html>
